eb5f09a2f5e0427c94a5d17ee86c2c4202a218c1
Richmond Kessie <kessierich.mag@gmail.com>
2022-11-04 23:12:50 +0000

    Kivy app

---
+++ README.md
@@ -0,0 +1,1 @@
+Dancing with Kivy for now
---
+++ sum.py
@@ -0,0 +1,30 @@
+# This program is about kivy-python
+# Running a simply calculator program
+
+import kivy
+import random
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+red = [1,0,0,1]
+green = [0,1,0,1]
+blue =  [0,0,1,1]
+purple = [1,0,1,1]
+
+class HBoxLayoutExample(App):
+    def build(self):
+        layout = BoxLayout(padding=10)
+        colors = [red, green, blue, purple]
+
+        for i in range(5):
+            btn = Button(text="Button #%s" % (i+1),
+                         background_color=random.choice(colors)
+                         )
+
+            layout.add_widget(btn)
+        return layout
+
+app = HBoxLayoutExample()
+app.run()
